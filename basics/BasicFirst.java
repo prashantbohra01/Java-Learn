5f97046e4e8ee1d15454e374f913c99a276ce4c4
Prashant Bohra <prashantb4157@gmail.com>
2024-07-08 21:43:29 +0530

    Delete basics directory

--- basics/BasicFirst.java
+++
@@ -1,9 +0,0 @@
-class BasicFirst{
-    public static void main(String[] args) {
-        System.out.println("HELLOOOOO");
-        System.out.print("YOOO");
-    }
-    }
-
-
-    // Single Line Comments  /* Multi Line comments */
--- basics/forLoop.java
+++
@@ -1,8 +0,0 @@
-public class forLoop {
-    public static void main(String[] args) {
-        for (int i = 0; i<10; i++){
-            System.out.println(i+"."+" Hello Prashant!");
-            System.out.println(10-i);
-        }
-    }
-}
